0e751ce92790789b6a3e89effa88f04a905be257
leefaisonr <rl3667@princeton.edu>
2023-03-22 14:17:38 -0400

    Include version-number in imagecat
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -12,3 +12,13 @@ set :branch, ENV['BRANCH'] || 'main'
 
 set :deploy_to, '/opt/imagecat-rails'
 # set :ssh_options, {verify_host_key: :never}
+
+desc "Write the current version to public/version.txt"
+task :write_version do
+  on roles(:app), in: :sequence do
+    within repo_path do
+      execute :tail, "-n1 ../revisions.log > #{release_path}/public/version.txt"
+    end
+  end
+end
+after "deploy:log_revision", "write_version"
