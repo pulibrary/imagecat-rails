4060cb4468d10c1f5b03b657f183cc911b7bb387
leefaisonr <rl3667@princeton.edu>
2023-04-19 11:02:42 -0400

    Install lux and vite
    Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com>

--- app/javascript/entrypoints/application.js
+++ app/javascript/entrypoints/application.js
@@ -26,3 +26,19 @@ console.log('Visit the guide for more information: ', 'https://vite-ruby.netlify
 
 // Example: Import a stylesheet in app/frontend/index.css
 // import '~/index.css'
+import Vue from "vue/dist/vue.esm"
+import system from "lux-design-system"
+import "lux-design-system/dist/system/system.css"
+import "lux-design-system/dist/system/tokens/tokens.scss"
+
+Vue.use(system)
+
+// create the LUX app and mount it to wrappers with class="lux"
+document.addEventListener("DOMContentLoaded", () => {
+  var elements = document.getElementsByClassName("lux")
+  for (var i = 0; i < elements.length; i++) {
+    new Vue({
+      el: elements[i]
+    })
+  }
+})
