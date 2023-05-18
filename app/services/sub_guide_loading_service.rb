4adc8445702c70ea2b4adbf0bee5d4c6217e9292
leefaisonr <rl3667@princeton.edu>
2023-05-18 17:29:36 -0400

    Add guide_card placeholder to save sub_guide_card
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>

--- app/services/sub_guide_loading_service.rb
+++ app/services/sub_guide_loading_service.rb
@@ -10,22 +10,23 @@ class SubGuideLoadingService
     @csv_location = csv_location || Rails.root.join('data', 'dbo-subguides', 'dbo.Subguides.17917.csv')
   end
 
-  def import
+  def import(parent_guide:)
     sub_guide_card_data = CSV.parse(File.read(csv_location), headers: true)
-    sub_guide_card_data.each do |entry|
-      import_sub_guide_card(entry)
+    sub_guide_card_data.each do |card|
+      import_sub_guide_card(card:, parent_guide:)
     end
   end
 
   private
 
-  def import_sub_guide_card(card)
+  def import_sub_guide_card(card:, parent_guide:)
     sgc = SubGuideCard.new
     sgc.id = card[0]
     sgc.heading = card[1]
     sgc.sortid = card[2]
     sgc.parentid = card[3]
     sgc.path = card[4]
+    sgc.guide_card = parent_guide
     sgc.save
   end
 end
