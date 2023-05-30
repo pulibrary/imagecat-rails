0968bf40f8f661da7957b69b4b3e950691ac2b5e
leefaisonr <rl3667@princeton.edu>
2023-05-30 15:19:53 -0400

    rubocop corrections

--- app/models/guide_card.rb
+++ app/models/guide_card.rb
@@ -4,9 +4,10 @@
 class GuideCard < ApplicationRecord
   paginates_per 10
   def image_count
-    image_dir="#{images_location.to_s}/disk#{path}"
+    image_dir = "#{images_location}/disk#{path}"
     Dir["#{image_dir}/*.tiff"].count
   end
+
   def images_location
     ENV['IMAGECAT_IMAGES'] || Rails.root.join('spec/fixtures/images')
   end
