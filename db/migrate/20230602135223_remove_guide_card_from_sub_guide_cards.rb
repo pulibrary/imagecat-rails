32bdf252cf7bf00e460f1a8340ea00aab625e0d4
leefaisonr <rl3667@princeton.edu>
2023-06-02 16:15:49 -0400

    rubocop corrections

--- db/migrate/20230602135223_remove_guide_card_from_sub_guide_cards.rb
+++ db/migrate/20230602135223_remove_guide_card_from_sub_guide_cards.rb
@@ -1,3 +1,6 @@
+# frozen_string_literal: true
+
+# db migration for revert of belong_to association with GuideCard
 class RemoveGuideCardFromSubGuideCards < ActiveRecord::Migration[7.0]
   def change
     remove_column :sub_guide_cards, :guide_card_id, :bigint
