f75cc9720be9620984f259077ecaa489205acf03
leefaisonr <rl3667@princeton.edu>
2023-06-05 11:40:53 -0400

    Create shell script to upload images to s3
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>

---
+++ data/copy_images.rb
@@ -0,0 +1,17 @@
+#!/usr/bin/env ruby
+
+# This is an example of what we want our script to look like for transferring image files from a source location to a destination.
+# ruby data/copy_images.rb /Users/faisonr/Downloads/imagecat-images-practice-1/input/disk14 s3://puliiif-staging
+source = ARGV[0]
+destination = ARGV[1]
+puts "Copying files from #{source} to #{destination}"
+files = Dir["#{source}/**/*.tiff"]
+files.each do |file_name|
+  puts file_name
+  file_parts = file_name.split("/")
+  file_parts[-1] = file_parts[-1].chop 
+  s3_safe_file_name = "imagecat-#{file_parts[-4..-1].join("-")}"
+  `aws s3 cp #{file_name} #{destination}/#{s3_safe_file_name}`
+end
+
+# "/Users/faisonr/Downloads/imagecat-images-practice-1/input/disk14/**/*.tiff"
