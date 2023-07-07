dafc8d41872f8ceae4f676e784c435ee83da0411
leefaisonr <rl3667@princeton.edu>
2023-07-07 10:12:30 -0400

    Use aws s3 sync for copying files to puliiif
    Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com>

--- data/copy_images.rb
+++ data/copy_images.rb
@@ -3,17 +3,19 @@
 
 # This is an example of what we want our script to look like for
 # transferring image files from a source location to a destination.
-# ruby data/copy_images.rb /Users/faisonr/Downloads/imagecat-images-practice-1/input/disk14 s3://puliiif-staging
+# ruby data/copy_images.rb /var/tmp/imagecat/disk1 s3://puliiif-staging
 source = ARGV[0]
 destination = ARGV[1]
 puts "Copying files from #{source} to #{destination}"
 files = Dir["#{source}/**/*.tiff"]
+sync_dir = File.join(source, 'for_aws_sync')
+Dir.mkdir(sync_dir)
 files.each do |file_name|
   puts file_name
   file_parts = file_name.split('/')
   file_parts[-1] = file_parts[-1].chop
   s3_safe_file_name = "imagecat-#{file_parts[-4..].join('-')}"
-  `aws s3 cp #{file_name} #{destination}/#{s3_safe_file_name}`
+  FileUtils.cp(file_name, File.join(sync_dir, s3_safe_file_name))
 end
-
-# "/Users/faisonr/Downloads/imagecat-images-practice-1/input/disk14/**/*.tiff"
+puts 'starting aws sync'
+`aws s3 sync #{sync_dir} #{destination}`
