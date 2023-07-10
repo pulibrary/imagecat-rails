fc3e126f11a8ab2735ba589836ff2e163ac35661
leefaisonr <rl3667@princeton.edu>
2023-07-10 11:42:49 -0400

    Restore this file to previous version for uploading

--- data/copy_images.rb
+++ data/copy_images.rb
@@ -1,8 +1,5 @@
-# frozen_string_literal: true
-
-require 'fileutils'
-
 # !/usr/bin/env ruby
+
 # frozen_string_literal: true
 
 # This is an example of what we want our script to look like for
@@ -12,14 +9,10 @@ source = ARGV[0]
 destination = ARGV[1]
 puts "Copying files from #{source} to #{destination}"
 files = Dir["#{source}/**/*.tiff"]
-sync_dir = File.join(source, 'for_aws_sync')
-Dir.mkdir(sync_dir)
 files.each do |file_name|
   puts file_name
   file_parts = file_name.split('/')
   file_parts[-1] = file_parts[-1].chop
   s3_safe_file_name = "imagecat-#{file_parts[-4..].join('-')}"
-  FileUtils.cp(file_name, File.join(sync_dir, s3_safe_file_name))
+  `aws s3 cp #{file_name} #{destination}/#{s3_safe_file_name}`
 end
-puts 'starting aws sync'
-`aws s3 sync #{sync_dir} #{destination}`
