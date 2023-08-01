8dafae26e0967fa0b9978465dd8b4b040320f9cc
leefaisonr <rl3667@princeton.edu>
2023-08-01 16:57:03 -0400

    switch to Firefox

--- spec/support/system_specs.rb
+++ spec/support/system_specs.rb
@@ -2,22 +2,21 @@
 
 RSpec.configure do |config|
   config.before(:each, type: :system) do
-    Webdrivers::Chromedriver.required_version = '114.0.5735.90'
     driven_by(:rack_test)
   end
 
   config.before(:each, type: :system, js: true) do
     if ENV['RUN_IN_BROWSER']
-      driven_by(:selenium_chrome)
+      driven_by(:selenium_firefox)
     else
-      driven_by(:selenium_chrome_headless)
+      driven_by(:selenium_firefox_headless)
     end
 
     # Make the screen larger so the save button is alway on screen. This avoids random `Element is not clickable` errors
     Capybara.page.driver.browser.manage.window.resize_to(2000, 2000)
   end
   config.before(:each, type: :system, js: true, in_browser: true) do
-    driven_by(:selenium_chrome)
+    driven_by(:selenium_firefox)
 
     # Make the screen larger so the save button is alway on screen. This avoids random `Element is not clickable` errors
     Capybara.page.driver.browser.manage.window.resize_to(2000, 2000)
