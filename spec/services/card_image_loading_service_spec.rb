f8c436da3e8d1242289d0288cc6027dd25a318c9
leefaisonr <rl3667@princeton.edu>
2023-08-10 17:03:16 -0400

    Start to import image files as CardImage objects
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>

--- spec/services/card_image_loading_service_spec.rb
+++ spec/services/card_image_loading_service_spec.rb
@@ -4,13 +4,20 @@ require 'rails_helper'
 
 describe CardImageLoadingService do
   let(:cils) { described_class.new }
+  let(:sgls) do
+    SubGuideLoadingService.new(csv_location: Rails.root.join('spec', 'fixtures', 'subguide_card_fixture.csv'))
+  end
+  let(:s3_response) do
+    "2023-07-19 14:39:38       3422 imagecat-disk9-0091-A3037-1358.0110.tif\n2023-07-19 14:39:38       7010 imagecat-disk9-0091-A3037-1358.0111.tif\n"
+  end
   it 'can instantiate' do
     expect(cils).to be_instance_of described_class
   end
 
   it 'imports all card images' do
+    sgls.import
     expect(CardImage.count).to eq 0
     cils.import
-    expect(CardImage.count).to eq 1
+    expect(CardImage.count).to eq 6
   end
 end
