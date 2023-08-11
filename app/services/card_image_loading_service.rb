c4c07264aa323c76a5f299380235c59192bec3e5
leefaisonr <rl3667@princeton.edu>
2023-08-11 16:26:27 -0400

    Fetch image file names from s3
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>

--- app/services/card_image_loading_service.rb
+++ app/services/card_image_loading_service.rb
@@ -9,7 +9,7 @@ class CardImageLoadingService
     SubGuideCard.all.each do |sgc|
       # Get an array of all file names on s3 that start that sgc.path
       # For each file name
-      image_array.each do |file_name|
+      image_array(sgc.path).each do |file_name|
         ci = CardImage.new
         ci.path = sgc.path
         ci.image_name = file_name
@@ -20,14 +20,15 @@ class CardImageLoadingService
 
   # returns something like
   # ["imagecat-disk9-0091-A3037-1358.0110.tif", "imagecat-disk9-0091-A3037-1358.0111.tif"]
-  def image_array
-    s3_image_list.split("\n").map(&:split).map(&:last)
+  def image_array(path)
+    s3_image_list(path).split("\n").map(&:split).map(&:last)
   end
 
   # returns something like
   # "2023-07-19 14:39:38       3422 imagecat-disk9-0091-A3037-1358.0110.tif\n2023-07-19 14:39:38       7010 imagecat-disk9-0091-A3037-1358.0111.tif\n"
-  def s3_image_list
-    # images_from_s3 = `aws s3 ls s3://puliiif-production/imagecat-disk9-0091-A3037-1358.011`
+  def s3_image_list(path)
+    s3_query = "aws s3 ls s3://puliiif-production/imagecat-disk#{path.tr('/', '-')}"
+    `#{s3_query}`
   end
 end
 # Next steps: to fill in s3_image_list method
--- spec/services/card_image_loading_service_spec.rb
+++ spec/services/card_image_loading_service_spec.rb
@@ -10,17 +10,26 @@ describe CardImageLoadingService do
   let(:s3_response) do
     "2023-07-19 14:39:38       3422 imagecat-disk9-0091-A3037-1358.0110.tif\n2023-07-19 14:39:38       7010 imagecat-disk9-0091-A3037-1358.0111.tif\n"
   end
+
+  before do
+    allow(cils).to receive(:s3_image_list).with(anything).and_return(s3_response)
+  end
+
   it 'can instantiate' do
     expect(cils).to be_instance_of described_class
   end
 
   it 'imports all card images' do
-    allow(cils).to receive(:s3_image_list).and_return(s3_response)
     sgls.import
     expect(CardImage.count).to eq 0
     cils.import
     expect(CardImage.count).to eq 12
     images = CardImage.where(path: '9/0091/A3037')
     expect(images.map(&:image_name)).to contain_exactly('imagecat-disk9-0091-A3037-1358.0110.tif', 'imagecat-disk9-0091-A3037-1358.0111.tif')
+  end
+
+  it 'gets a list of images from s3' do
+    image_list = cils.s3_image_list('9/0091/A3037')
+    expect(image_list.split("\n").count).to eq 2
   end
 end
