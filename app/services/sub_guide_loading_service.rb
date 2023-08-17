9c7a8ebc875ff9bd21f736f348adaec52a396aa1
leefaisonr <rl3667@princeton.edu>
2023-08-17 17:28:11 -0400

    Added and colorfied ruby progress bar
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>

--- app/services/sub_guide_loading_service.rb
+++ app/services/sub_guide_loading_service.rb
@@ -4,22 +4,26 @@ require 'csv'
 require 'ruby-progressbar'
 # service for loading SubGuide cards data
 class SubGuideLoadingService
-  attr_reader :csv_location
+  attr_reader :csv_location, :progressbar
 
   # @param csv_location [String] location of source data for SubGuideCards
-  def initialize(csv_location: nil)
+  def initialize(csv_location: nil, progressbar: nil)
     @csv_location = csv_location || Rails.root.join('data', 'dbo-subguides', 'dbo.Subguides.17917.csv')
+    @progressbar = progressbar || ProgressBar.create(format: "\e[1;35m%t: |%B|\e[0m")
   end
 
   def import
     sub_guide_card_data = CSV.parse(File.read(csv_location), headers: true, liberal_parsing: true)
-    progressbar = ProgressBar.create
-    pb_increment = sub_guide_card_data.count / 100
+    @progressbar.total = sub_guide_card_data.count
     sub_guide_card_data.each_with_index do |entry, index|
-      byebug
-      2.times { progressbar.increment }
+      progress_bar_random_color if (index % 100).zero?
+      @progressbar.increment
       import_sub_guide_card(entry)
     end
+  end
+
+  def progress_bar_random_color
+    @progressbar.format = "%t: |\e[#{rand(91..97)}m%B\e[0m|"
   end
 
   private
--- spec/services/sub_guide_loading_service_spec.rb
+++ spec/services/sub_guide_loading_service_spec.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
+require 'ruby-progressbar/outputs/null'
 
 describe SubGuideLoadingService do
-  let(:sgls) { described_class.new(csv_location: Rails.root.join('spec', 'fixtures', 'subguide_card_fixture.csv')) }
+  let(:sgls) do
+    described_class.new(csv_location: Rails.root.join('spec', 'fixtures', 'subguide_card_fixture.csv'),
+                        progressbar: ProgressBar.create(output: ProgressBar::Outputs::Null))
+  end
   it 'can instantiate' do
     expect(sgls).to be_instance_of described_class
   end
@@ -18,11 +22,9 @@ describe SubGuideLoadingService do
     expect(SubGuideCard.count).to eq 7
   end
 
-  xit 'displays progress status during import' do
-    expect { sgls.import }.to output("#######task completed!\n").to_stdout
-  end
-
   it 'displays ruby-progress bar during import' do
-    expect { sgls.import }.to output(/Progress: /).to_stdout_from_any_process
+    expect(sgls.progressbar.to_h['percentage']).to eq 0
+    sgls.import
+    expect(sgls.progressbar.to_h['percentage']).to eq 100
   end
 end
