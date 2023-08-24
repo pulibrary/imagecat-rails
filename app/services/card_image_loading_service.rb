84d6b2e73e494f3a2222d9fd3b63cffc34747100
leefaisonr <rl3667@princeton.edu>
2023-08-24 16:08:09 -0400

    Adds Ruby progress bar to card image import service
    Co-authored-by: Jane Sandberg <sandbergja@users.noreply.github.com>

--- app/services/card_image_loading_service.rb
+++ app/services/card_image_loading_service.rb
@@ -1,24 +1,31 @@
 # frozen_string_literal: true
 
+require 'ruby-progressbar'
+
 # Class for card image loading service
 class CardImageLoadingService
+  attr_reader :progressbar
+
+  def initialize(progressbar: nil)
+    @progressbar = progressbar || ProgressBar.create(format: "\e[1;35m%t: |%B|\e[0m")
+  end
+
   # For each SubGuideCard, take its path and query s3 to get all of the image names
   # for that path. For each image file, create a CardImage object with the path and
   # image name.
+  
   def import
-    SubGuideCard.all.each do |sgc|
-      # Get an array of all file names on s3 that start that sgc.path
-      # For each file name
+    @progressbar.total = SubGuideCard.all.count
+    SubGuideCard.all.each_with_index do |sgc, index|
+      progress_bar_random_color if (index % 100).zero?
+      @progressbar.increment
       image_array(sgc.path).each do |file_name|
         ci = CardImage.new
         ci.path = sgc.path
         ci.image_name = file_name
         ci.save
-        print '#'
-        $stdout.flush
       end
     end
-    puts 'task completed!'
   end
 
   # returns something like
@@ -32,5 +39,9 @@ class CardImageLoadingService
   def s3_image_list(path)
     s3_query = "aws s3 ls s3://puliiif-production/imagecat-disk#{path.tr('/', '-')}"
     `#{s3_query}`
+  end
+
+  def progress_bar_random_color
+    @progressbar.format = "%t: |\e[#{rand(91..97)}m%B\e[0m|"
   end
 end
