4bb500003efc01017e462d466a908305491de58f
leefaisonr <rl3667@princeton.edu>
2023-09-06 19:11:08 -0400

    Add pagination to SubGuide cards
    Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com>

--- app/controllers/sub_guide_cards_controller.rb
+++ app/controllers/sub_guide_cards_controller.rb
@@ -6,6 +6,6 @@ class SubGuideCardsController < ApplicationController
 
   def show
     @sub_guide_card = SubGuideCard.find(params[:id])
-    @card_images = CardImage.where(path: @sub_guide_card.path)
+    @card_images = CardImage.where(path: @sub_guide_card.path).page(params[:page])
   end
 end
