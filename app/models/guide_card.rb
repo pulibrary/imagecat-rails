7ea1284c0bb6265348ab593411e1464a8687eb42
leefaisonr <rl3667@princeton.edu>
2023-09-08 09:53:40 -0400

    Finished non-matching-search function
    Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com>

--- app/models/guide_card.rb
+++ app/models/guide_card.rb
@@ -5,6 +5,15 @@ class GuideCard < ApplicationRecord
   paginates_per 10
   include HasChildren
 
+  # First, try to find an exact match. Otherwise, find the closest match.
+  def self.search_result(term)
+    match = GuideCard.find_by(heading: term)
+    return match if match.present?
+
+    GuideCard.where('heading < ?', term).order(heading: :desc).limit(1).first
+  end
+
+  # Calculate which page of the full index list contains the given GuideCard.
   def index_page
     number_to_check = id - 1
     (number_to_check / 10) + 1
