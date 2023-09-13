0d8dd145e1ef3308efdaf44c052a1b86034c2806
leefaisonr <rl3667@princeton.edu>
2023-09-13 09:56:47 -0400

    Finalized card image pagination
    Co-authored-by: Anna Headley <hackartisan@users.noreply.github.com>

--- app/models/sub_guide_card.rb
+++ app/models/sub_guide_card.rb
@@ -3,7 +3,6 @@
 # SubGuideCards provide hierarchical structure between GuideCards and catalog cards
 class SubGuideCard < ApplicationRecord
   # A sub_guide_card parent can either be a GuideCard or a SubGuideCard
-  paginates_per 5
   def parent
     GuideCard.find_by(sortid: parentid) || SubGuideCard.find_by(sortid: parentid)
   end
