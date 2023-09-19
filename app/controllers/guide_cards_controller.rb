abbdf4c9dc68f6df7cb5b6ebfc3bce1ecc28657c
leefaisonr <rl3667@princeton.edu>
2023-09-19 10:51:23 -0400

    Be able to paginate search results
    Note that these cards must be ordered by id, not heading. This is
    counterintuitive, because it results in a thing that might not look like
    alphabetical order. However, it is the order of the card catalog, which
    is wanted by the stakeholder.
    
    Co-authored-by: Bess Sadler <bess@users.noreply.github.com>

--- app/controllers/guide_cards_controller.rb
+++ app/controllers/guide_cards_controller.rb
@@ -3,16 +3,16 @@
 # controller for GuideCards
 class GuideCardsController < ApplicationController
   def index
-    @guide_cards = GuideCard.order(:heading).page(params[:page])
+    @guide_cards = GuideCard.order(:id).page(params[:page])
   end
 
   def search
     @match = GuideCard.search_result(params[:search_term])
     @guide_cards =
       if params[:page].present?
-        GuideCard.order(:heading).page(params[:page])
+        GuideCard.order(:id).page(params[:page])
       else
-        GuideCard.order(:heading).page(@match.index_page)
+        GuideCard.order(:id).page(@match.index_page)
       end
   end
 
