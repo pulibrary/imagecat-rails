b4d3c7e9436b9763b6031a261d21de066b72de50
Anna Headley <845363+hackartisan@users.noreply.github.com>
2023-10-10 12:31:02 -0400

    Refactor progress bar

--- spec/services/card_image_loading_service_spec.rb
+++ spec/services/card_image_loading_service_spec.rb
@@ -1,17 +1,20 @@
 # frozen_string_literal: true
 
 require 'rails_helper'
-require 'ruby-progressbar/outputs/null'
 
 describe CardImageLoadingService do
-  let(:cils) { described_class.new(progressbar: ProgressBar.create(output: ProgressBar::Outputs::Null)) }
+  let(:cils) { described_class.new(suppress_progress: true) }
   let(:gcls) do
-    GuideCardLoadingService.new(csv_location: Rails.root.join('spec', 'fixtures', 'guide_card_fixture.csv'),
-                                progressbar: ProgressBar.create(output: ProgressBar::Outputs::Null))
+    GuideCardLoadingService.new(
+      csv_location: Rails.root.join('spec', 'fixtures', 'guide_card_fixture.csv'),
+      progressbar: ProgressBar.create(output: ProgressBar::Outputs::Null)
+    )
   end
   let(:sgls) do
-    SubGuideLoadingService.new(csv_location: Rails.root.join('spec', 'fixtures', 'subguide_card_fixture.csv'),
-                               progressbar: ProgressBar.create(output: ProgressBar::Outputs::Null))
+    SubGuideLoadingService.new(
+      csv_location: Rails.root.join('spec', 'fixtures', 'subguide_card_fixture.csv'),
+      progressbar: ProgressBar.create(output: ProgressBar::Outputs::Null)
+    )
   end
   let(:s3_response) do
     "2023-07-19 14:39:38       3422 imagecat-disk9-0091-A3037-1358.0110.tif\n2023-07-19 14:39:38       7010 imagecat-disk9-0091-A3037-1358.0111.tif\n"
@@ -21,10 +24,6 @@ describe CardImageLoadingService do
     allow(cils).to receive(:s3_image_list).with(anything).and_return(s3_response)
     # Any card with a path of "sub" will not have any images
     allow(cils).to receive(:s3_image_list).with('sub').and_return('')
-  end
-
-  it 'can instantiate' do
-    expect(cils).to be_instance_of described_class
   end
 
   it 'imports all SubGuideCard images' do
@@ -50,12 +49,6 @@ describe CardImageLoadingService do
   it 'gets a list of images from s3' do
     image_list = cils.s3_image_list('9/0091/A3037')
     expect(image_list.split("\n").count).to eq 2
-  end
-
-  it 'displays ruby-progress bar during import' do
-    expect(cils.progressbar.to_h['percentage']).to eq 0
-    cils.import_sub_guide_images
-    expect(cils.progressbar.to_h['percentage']).to eq 100
   end
 
   it 'imports both GuideCard and SubGuideCard images' do
