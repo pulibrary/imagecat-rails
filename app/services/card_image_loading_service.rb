95b4009c3daddbc2062269b62c1ea5745728aa13
Anna Headley <845363+hackartisan@users.noreply.github.com>
2023-10-10 14:20:43 -0400

    Refactor CardImageLoadingService to use aws ls per disk
    instead of aws ls per database object
    
    closes #188

--- app/services/card_image_loading_service.rb
+++ app/services/card_image_loading_service.rb
@@ -12,60 +12,40 @@ class CardImageLoadingService
   end
 
   def import
-    import_guide_card_images
-    import_sub_guide_images
+    (1..22).each { |disk| import_disk(disk) }
   end
 
-  # For each SubGuideCard, take its path and query s3 to get all of the image names
-  # for that path. For each image file, create a CardImage object with the path and
-  # image name.
-  def import_sub_guide_images
-    progressbar = progress_bar(SubGuideCard.count)
-    SubGuideCard.all.each do |sgc|
-      progressbar.increment
-      image_array(sgc.path).each do |file_name|
-        create_card_image(sgc, file_name)
-      end
+  def import_disk(disk)
+    disk_array(disk).each do |file_name|
+      find_or_create_card_image(file_name)
     end
-  end
-
-  def import_guide_card_images
-    progressbar = progress_bar(GuideCard.count)
-    GuideCard.all.each do |gc|
-      progressbar.increment
-      image_array(gc.path).each do |file_name|
-        create_card_image(gc, file_name)
-      end
-    end
-  end
-
-  # returns something like
-  # ["imagecat-disk9-0091-A3037-1358.0110.tif", "imagecat-disk9-0091-A3037-1358.0111.tif"]
-  def image_array(path)
-    s3_image_list(path).split("\n").map(&:split).map(&:last)
-  end
-
-  # returns something like
-  # "2023-07-19 14:39:38       3422 imagecat-disk9-0091-A3037-1358.0110.tif\n2023-07-19 14:39:38       7010 imagecat-disk9-0091-A3037-1358.0111.tif\n"
-  def s3_image_list(path)
-    s3_query = "aws s3 ls s3://puliiif-production/imagecat-disk#{path.tr('/', '-')}"
-    `#{s3_query}`
   end
 
   private
 
-  def create_card_image(sgc, file_name)
-    ci = CardImage.find_by(path: sgc.path, image_name: file_name)
+  # returns something like
+  # ["imagecat-disk9-0091-A3037-1358.0110.tif", "imagecat-disk9-0091-A3037-1358.0111.tif"]
+  def disk_array(disk)
+    s3_disk_list(disk).split("\n").map(&:split).map(&:last)
+  end
+
+  # returns something like
+  # "2023-07-19 14:39:38       3422 imagecat-disk9-0091-A3037-1358.0110.tif\n2023-07-19 14:39:38       7010 imagecat-disk9-0091-A3037-1358.0111.tif\n"
+  def s3_disk_list(disk)
+    s3_query = "aws s3 ls s3://puliiif-production/imagecat-disk#{disk}"
+    `#{s3_query}`
+  end
+
+  def find_or_create_card_image(file_name)
+    path = file_name.gsub('imagecat-disk', '').split('-')[0..-2].join('/')
+    ci = CardImage.find_by(path:, image_name: file_name)
     return if ci
 
-    ci = CardImage.new
-    ci.path = sgc.path
-    ci.image_name = file_name
-    ci.save
+    CardImage.create(path:, image_name: file_name)
   end
 
   def progress_bar(total)
-    ProgressBar.create(format: "\e[1;35m%t: |%B|\e[0m", total: total, output: progress_output)
+    ProgressBar.create(format: "\e[1;35m%t: |%B|\e[0m", total:, output: progress_output)
   end
 
   def progress_output
